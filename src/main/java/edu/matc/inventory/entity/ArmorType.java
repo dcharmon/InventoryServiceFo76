c79f3cc1baa77d91ab4c1d813767c8aba24e86ac
dcharmon <dcorbinharmon@gmail.com>
2026-02-22 17:15:58 -0600

    Add ArmorType and ArmorSlot entities for ManyToOne relationships

--- src/main/java/edu/matc/inventory/entity/ArmorType.java
+++ src/main/java/edu/matc/inventory/entity/ArmorType.java
@@ -1,7 +1,10 @@
 package edu.matc.inventory.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 
+/**
+ * Represents an armor type (e.g., Leather armor, Combat armor).
+ */
 @Entity
 @Table(name = "armor_type")
 public class ArmorType {
@@ -11,21 +14,41 @@ public class ArmorType {
     @Column(name = "armor_type_id")
     private int id;
 
-    @Column(name = "type_name")
+    @Column(name = "type_name", nullable = false)
     private String typeName;
 
+    /**
+     * Gets id.
+     *
+     * @return armor type id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id armor type id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets type name.
+     *
+     * @return armor type name
+     */
     public String getTypeName() {
         return typeName;
     }
 
+    /**
+     * Sets type name.
+     *
+     * @param typeName armor type name
+     */
     public void setTypeName(String typeName) {
         this.typeName = typeName;
     }
